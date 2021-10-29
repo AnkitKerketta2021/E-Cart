f4854497ee956b986989ba04d085b0d0ee27822f
AnkitKerketta2021 <ankitkerketta999.com>
2021-10-29 22:50:08 +0530

    Merge 1.1

--- E-cart/src/App.js
+++ E-cart/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Body from './components/mainPage/body/Body';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/navbar/NavBar';
+import SecondNav from './components/mainPage/top/SecondNav';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SecondNav />
       <Body />
     </div>
   );
