6552baba70d393e4f4605bf861d8ca955f2792ca
AnkitKerketta2021 <ankitkerketta999.com>
2021-10-29 22:33:49 +0530

    Merging 1.0

--- E-cart/src/App.js
+++ E-cart/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Body from './components/mainPage/body/Body';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Body />
     </div>
   );
--- E-cart/src/components/mainPage/body/Products.js
+++ E-cart/src/components/mainPage/body/Products.js
@@ -1,23 +1,27 @@
 import React from "react";
 import './Products.css'
+import { Button } from 'react-bootstrap'
 
 function Products(props) {
     return (
-            <div id="container">
-                    <div className="cards">
-                        <img
-                            width="220px" height='220px'
-                            src={props.img} alt={props.id} />
-                    </div>
-                    <br />
-                    <div className="">
-                        <span>{props.name}</span>
-                    </div>
-                    <div className="ps-2">
-                        <b style={{ color: 'red' }}>Rs. {props.price}</b>
-                    </div>
-                    <br />
-                </div>
+        <div id="container">
+            <div className="cards">
+                <img
+                    width="220px" height='220px'
+                    src={props.img} alt={props.id} />
+            </div>
+            <br />
+            <div className="">
+                <span>{props.name}</span>
+            </div>
+            <div className="ps-2">
+                <h6 style={{
+                    color: "#f00"
+                }}>₹ {props.price}</h6>
+            </div>
+            <div><Button id="button" variant="warning">Add to Cart</Button></div>
+            <br />
+        </div>
     );
 }
 export default Products;
